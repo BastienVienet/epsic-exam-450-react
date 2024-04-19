ef3488a8d1b3fb44183f78dab84f4404611b7869
BastienVienet <bastien.vienet@tipee.ch>
2024-04-19 09:03:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# To-do List
+
+## Fonctionalités de notre to-do list
+
+- Ajouter une tâche
+- Editer une tâche
+- Supprimer une tâche
+- Marquer une tâche comme complétée (mais sans la supprimer)
+- Filtrer les tâches pour cacher les tâches terminées
+- Lorsque je rafraichis la page, mes Tâches sont encore présentes dans le même état qu’avant de rafraîchir
+
+## Installation du projet
+
+1. Cloner le projet
+2. Installer les dépendances avec `npm install`
+3. Lancer le projet avec `npm start`
---
+++ src/components/ToDoSetter.tsx
@@ -0,0 +1,28 @@
+import { KeyboardEvent } from "react";
+import StyledButton from "../styles/Button";
+import StyledInput from "../styles/Input";
+
+type Props = {
+  onAdd: (input: string) => void;
+  input: string;
+  setInput: (value: string) => void;
+};
+
+export const ToDoSetter = ({ onAdd, input, setInput }: Props) => {
+  const handleEnterPress = (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+      onAdd(input);
+    }
+  };
+  return (
+    <>
+      <StyledInput
+        onKeyDown={handleEnterPress}
+        type="text"
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <StyledButton onClick={() => onAdd(input)}>Add</StyledButton>
+    </>
+  );
+};
