6c2cb5008ca58139e23b2b4a7371fdf2178ca96b
BastienVienet <bastien.vienet@tipee.ch>
2024-05-01 18:49:10 +0200

    unit-test-deleteToDo

--- src/hooks/__tests__/useToDo.test.tsx
+++ src/hooks/__tests__/useToDo.test.tsx
@@ -28,3 +28,23 @@ it("should add a new todo to the todo list", () => {
     { id: expect.any(Number), text: "Ajouter une nouvelle tâche", completed: false },
   ]);
 });
+
+// Unit Test
+it("should delete a todo from the todo list", () => {
+  // Arrange: Set up the mock function and the props for the hook
+  const spy = jest.fn();
+  const props = {
+    setToDoList: spy,
+    toDoList: mockTodos,
+    setInput: () => {},
+  };
+
+  // Act: Render the hook and call the deleteToDo function
+  const { result } = renderHook(() => useToDo(props));
+  result.current.deleteToDo(1);
+
+  // Assert: Check that the setToDoList function was called with the correct arguments
+  expect(spy).toHaveBeenCalledWith([
+    { id: 2, text: "Prendre des vacances", completed: false },
+  ]);
+});
